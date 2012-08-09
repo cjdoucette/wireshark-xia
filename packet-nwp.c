a99f34f748dade714f9d6d7423c40c12d2a92a36
Cody Doucette <doucette@bu.edu>
2012-08-09 14:44:10 -0400

    Initial commit.

---
+++ packet-nwp.c
@@ -0,0 +1,350 @@
+/* packet-nwp.c
+ * Routines for NWP dissection.
+ * Copyright 2012, Cody Doucette <doucette@bu.edu>
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <epan/packet.h>
+#include "packet-xip-dag.h"
+
+typedef unsigned char 	u8;
+
+#define XIDTYPE_HID 	(__cpu_to_be32(0x11))
+#define XID_LEN		20
+#define NWPH_MIN_LEN	36
+
+#define NWP_VERSION	0x01
+
+/*
+ * Note: To add a new NWP packet type, include:
+ *
+ * * A new type definition directive.
+ * * A new entry in the map_types function.
+ * * A dissect_nwp_<new_type> function.
+ * * A new entry in dissect_nwp that calls the above function.
+ *
+ */
+
+#define NWP_TYPE_ANNOUNCEMENT	0x01
+#define NWP_TYPE_NEIGH_LIST	0x02
+#define NWP_TYPE_MAX		0x03
+
+#define ETHERTYPE_NWP		0xC0DF	   /* Neighborhood Watch Protocol */
+				        /* [NOT AN OFFICIALLY REGISTERED ID] */
+
+/* Offsets of fields within a NWP header. */
+#define NWPH_VERS	0
+#define NWPH_TYPE	1
+#define NWPH_HIDC	2
+#define NWPH_HLEN	3
+
+#define NWPH_HWAD	4
+#define NWPH_NLST	4
+
+static int proto_nwp = -1;
+
+/* Header fields for all NWP headers. */
+static int hf_nwp_version = -1;
+static int hf_nwp_type = -1;
+static int hf_nwp_hid_count = -1;
+static int hf_nwp_haddr_len = -1;
+
+/* Header fields for NWP Announcement packets. */
+static int hf_nwp_haddr = -1;
+static int hf_nwp_hids = -1;
+static int hf_nwp_hid = -1;
+
+/* Header fields for NWP Neighbor List packets. */
+static int hf_nwp_neigh_list = -1;
+static int hf_nwp_neigh = -1;
+static int hf_nwp_neigh_ha = -1;
+
+/* Subtrees. */
+static gint ett_nwp = -1;
+static gint ett_nwp_ann_hids = -1;
+static gint ett_nwp_neigh_list = -1;
+
+/* Convert a stream of u8 bytes to a XIA XID. */
+static gchar *
+str_of_xid(gchar **dest_str, u8 *id)
+{
+	struct xia_xid xid;
+	*dest_str = (char *)ep_alloc(XIA_MAX_STRXID_SIZE + 1);
+	xid.xid_type = XIDTYPE_HID;
+	memcpy(xid.xid_id, id, XIA_XID_MAX);
+	xia_xidtop(&xid, (gchar *)*dest_str, XIA_MAX_STRXID_SIZE);
+}
+
+/* Map a type number to a type string for display. */
+static gchar *
+map_types(guint8 type)
+{
+	switch (type) {
+	case NWP_TYPE_ANNOUNCEMENT:
+		return "NWP Announcement (0x01)";
+	case NWP_TYPE_NEIGH_LIST:
+		return "NWP Neighborhood List (0x02)";
+	default:
+		return "Invalid NWP packet type";
+	}
+}
+
+gchar *
+tvb_nwphrdaddr_to_str(tvbuff_t *tvb, gint offset, int ad_len)
+{
+	if (ad_len == 0)
+		return "<No address>";
+	if (ad_len == 6)
+		return tvb_ether_to_str(tvb, offset);
+
+	return tvb_bytes_to_str(tvb, offset, ad_len);
+}
+
+/* Process the neighbor list in a NWP packet. */
+static void
+process_neighs(proto_tree *list_tree, tvbuff_t *tvb, guint8 ha_len)
+{
+	guint8 ha_count, i, j, offset, hid_count;
+	gchar *byte_str, *xid_str, *ha, *ha_begin;
+
+	offset = NWPH_NLST;
+	hid_count = tvb_get_guint8(tvb, NWPH_HIDC);
+	byte_str = (gchar *)ep_alloc(2*XID_LEN + 1);
+	ha = (gchar *)ep_alloc(2*ha_len + 1);
+	ha_begin = ha;
+
+	for (i = 0; i < hid_count; i++) {
+
+		byte_str = tvb_get_string(tvb, offset, XID_LEN);
+		str_of_xid(&xid_str, (u8 *)byte_str);
+
+		proto_tree_add_string_format(list_tree, hf_nwp_neigh,
+		       tvb, offset, XID_LEN, xid_str, "%s", xid_str);
+
+		offset += XID_LEN;
+		ha_count = tvb_get_guint8(tvb, offset);
+		offset++;
+
+		for (j = 0; j < ha_count; j++) {
+
+			ha = tvb_nwphrdaddr_to_str(tvb, offset, ha_len);
+			proto_tree_add_string_format(list_tree, hf_nwp_haddr,
+				    		     tvb, offset, ha_len, ha,
+					            "    %d: %s", j + 1, ha);
+			offset += ha_len;
+		}
+	}
+}
+
+/* Dissector for NWP Announcement packets. */
+static void
+dissect_nwp_ann(tvbuff_t *tvb, packet_info *pinfo, proto_tree *nwp_tree,
+		guint8 ha_len)
+{
+	proto_tree *hid_tree = NULL;
+	proto_item *ti = NULL;
+
+	gchar *byte_str, *xid_str, *ha, *ha_begin;
+	guint8 hid_count, i;
+
+	guint8 off = 0;
+	byte_str = (gchar *)ep_alloc(2*XID_LEN + 1);
+	ha = (gchar *)ep_alloc(2*ha_len + 1);
+	ha_begin = ha;
+
+	hid_count = tvb_get_guint8(tvb, NWPH_HIDC);
+
+	col_add_str(pinfo->cinfo, COL_INFO, "NWP Announcement");
+
+	ha = tvb_nwphrdaddr_to_str(tvb, NWPH_HWAD, ha_len);
+
+	proto_tree_add_string_format(nwp_tree, hf_nwp_haddr, tvb, NWPH_HWAD,
+			  	    ha_len, ha, "Hardware Address: %s", ha);
+
+	ti = proto_tree_add_item(nwp_tree, hf_nwp_hids, tvb,
+		    NWPH_HWAD + ha_len, -1, ENC_BIG_ENDIAN);
+
+	hid_tree = proto_item_add_subtree(ti, ett_nwp_ann_hids);
+
+	for (i = 0; i < hid_count; i++) {
+
+		byte_str = tvb_get_string(tvb, NWPH_HWAD + ha_len + off,
+							       XID_LEN);
+		str_of_xid(&xid_str, (u8 *)byte_str);
+
+		proto_tree_add_string_format(hid_tree, hf_nwp_hid, tvb,
+		      NWPH_HWAD + ha_len + off, XID_LEN, xid_str, "%s",
+							      xid_str);
+		off += XID_LEN;
+	}
+}
+
+/* Dissector for NWP Neighbor List packets. */
+static void
+dissect_nwp_nl(tvbuff_t *tvb, packet_info *pinfo, proto_tree *nwp_tree,
+	       guint8 ha_len)
+{
+	proto_tree *list_tree = NULL;
+	proto_item *ti = NULL;
+
+	col_add_str(pinfo->cinfo, COL_INFO, "NWP Neighbor List");
+
+	ti = proto_tree_add_item(nwp_tree, hf_nwp_neigh_list,
+		 	 tvb, NWPH_NLST, -1, ENC_BIG_ENDIAN);
+
+	list_tree = proto_item_add_subtree(ti, ett_nwp_neigh_list);
+
+	process_neighs(list_tree, tvb, ha_len);
+}
+
+static int
+dissect_nwp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+	proto_tree *nwp_tree = NULL;
+	proto_item *ti = NULL;
+
+	guint8 type;
+	guint *type_str;
+	guint8 ha_len;
+
+	type = tvb_get_guint8(tvb, NWPH_TYPE);
+	type_str = map_types(type);
+	ha_len = tvb_get_guint8(tvb, NWPH_HLEN);
+
+	col_set_str(pinfo->cinfo, COL_PROTOCOL, "NWP");
+	col_clear(pinfo->cinfo, COL_INFO);
+
+	if (tree) {
+
+		/* Construct protocol tree. */
+		ti = proto_tree_add_item(tree, proto_nwp, tvb, 0, -1, ENC_NA);
+
+		nwp_tree = proto_item_add_subtree(ti, ett_nwp);
+
+		/* Add header fields to tree. */
+		proto_tree_add_item(nwp_tree, hf_nwp_version, tvb,
+				    NWPH_VERS, 1, ENC_BIG_ENDIAN);
+
+		proto_tree_add_string(nwp_tree, hf_nwp_type, tvb,
+				    NWPH_TYPE, 1, type_str);
+
+		proto_tree_add_item(nwp_tree, hf_nwp_hid_count, tvb,
+				    NWPH_HIDC, 1, ENC_BIG_ENDIAN);
+
+		proto_tree_add_item(nwp_tree, hf_nwp_haddr_len, tvb,
+				    NWPH_HLEN, 1, ENC_BIG_ENDIAN);
+
+		switch (type) {
+
+		case NWP_TYPE_ANNOUNCEMENT:
+
+			dissect_nwp_ann(tvb, pinfo, nwp_tree, ha_len);
+			break;
+
+		case NWP_TYPE_NEIGH_LIST:
+
+			dissect_nwp_nl(tvb, pinfo, nwp_tree, ha_len);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	return tvb_length(tvb);
+}
+
+void
+proto_register_nwp(void)
+{
+	static hf_register_info hf[] = {
+
+		{ &hf_nwp_version,
+		{ "Version", "nwp.version", FT_UINT8,
+		   BASE_DEC, NULL, 0x0,	NULL, HFILL }},
+
+		{ &hf_nwp_type,
+		{ "Type", "nwp.type", FT_STRINGZ,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_nwp_hid_count,
+		{ "HID Count", "nwp.hid_count", FT_UINT8,
+		   BASE_DEC, NULL, 0x0,	NULL, HFILL }},
+
+		{ &hf_nwp_haddr_len,
+		{ "Hardware Address Length", "nwp.haddr_len", FT_UINT8,
+		   BASE_DEC, NULL, 0x0,	NULL, HFILL }},
+
+		{ &hf_nwp_haddr,
+		{ "Hardware Address", "nwp.haddr", FT_STRING,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_nwp_hids,
+		{ "HIDs", "nwp.hids", FT_NONE,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_nwp_hid,
+		{ "HID", "nwp.hid", FT_STRING,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_nwp_neigh_list,
+		{ "Neighbor List", "nwp.neigh_list", FT_NONE,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_nwp_neigh,
+		{ "Neighbor", "nwp.neigh", FT_STRING,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_nwp_neigh_ha,
+		{ "Neighbor Hardware Address", "nwp.neigh_ha", FT_STRING,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }}
+
+	};
+
+	static gint *ett[] = {
+		&ett_nwp,
+		&ett_nwp_ann_hids,
+		&ett_nwp_neigh_list
+	};
+
+
+	proto_nwp = proto_register_protocol(
+		"Neighborhood Watch Protocol",
+		"NWP",
+	        "nwp");
+
+	proto_register_field_array(proto_nwp, hf, array_length(hf));
+	proto_register_subtree_array(ett, array_length(ett));
+}
+
+void
+proto_reg_handoff_nwp(void)
+{
+	dissector_handle_t nwp_handle;
+	nwp_handle = new_create_dissector_handle(dissect_nwp, proto_nwp);
+	dissector_add_uint("ethertype", ETHERTYPE_NWP, nwp_handle);
+}
+
---
+++ packet-xip.c
@@ -0,0 +1,269 @@
+/* packet-xip.c
+ * Routines for XIP dissection
+ * Copyright 2012, Cody Doucette <doucette@bu.edu>
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <epan/packet.h>
+#include <ipproto.h>
+#include "packet-xip-dag.h"
+#include "packet-xip-dag-userland.h"
+
+/* Minimum XIP header length. */
+#define XIPH_MIN_LEN	36
+
+/* Size of a DAG node. */
+#define NODE_SIZE	28
+
+/* XIP Ethertype. */
+#define ETHERTYPE_XIP	0xC0DE     /* eXpressive Internet Protocol */
+				/* [NOT AN OFFICIALLY REGISTERED ID] */
+
+/* Offsets of fields within an XIP header. */
+#define XIPH_VERS	0
+#define XIPH_NXTH	1
+#define XIPH_PLEN	2
+#define XIPH_HOPL	4
+#define XIPH_NDST	5
+#define XIPH_NSRC	6
+#define XIPH_LSTN	7
+#define XIPH_DSTD	8
+
+static int proto_xip = -1;
+
+static int hf_xip_version = -1;
+static int hf_xip_next_hdr = -1;
+static int hf_xip_payload_len = -1;
+static int hf_xip_hop_limit = -1;
+static int hf_xip_num_dst = -1;
+static int hf_xip_num_src = -1;
+static int hf_xip_last_node = -1;
+static int hf_xip_dst_dag = -1;
+static int hf_xip_dst_dag_entry = -1;
+static int hf_xip_src_dag = -1;
+static int hf_xip_src_dag_entry = -1;
+
+static gint ett_xip = -1;
+static gint ett_xip_ddag = -1;
+static gint ett_xip_sdag = -1;
+
+static dissector_handle_t udp_handle;
+
+void
+display_dag(proto_tree *tr, int hf, tvbuff_t *tvb, guint8 off, char *buf)
+{
+	guint8 i = 0;
+	char *p =  strtok(buf, "\n");
+
+	while (p != NULL) {
+		proto_tree_add_string_format(tr, hf, tvb, off + (i * NODE_SIZE),
+							NODE_SIZE, p, "%s", p);
+		p = strtok(NULL, "\n");
+		i++;
+	}
+}
+
+static int
+dissect_xip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+	struct xia_addr dst;
+	struct xia_addr src;
+
+	char dst_string[XIA_MAX_STRADDR_SIZE];
+	char src_string[XIA_MAX_STRADDR_SIZE];
+
+	proto_tree *xip_tree = NULL;
+	proto_tree *dst_tree = NULL;
+	proto_tree *src_tree = NULL;
+
+	proto_item *ti = NULL;
+
+	tvbuff_t *next_tvb;
+
+	guint8 dst_nodes = tvb_get_guint8(tvb, XIPH_NDST);
+	guint8 src_nodes = tvb_get_guint8(tvb, XIPH_NSRC);
+	guint8 src_offset = XIPH_DSTD + (NODE_SIZE * dst_nodes);
+	guint32 hlen = 8 + (NODE_SIZE * dst_nodes) + (NODE_SIZE * src_nodes);
+	guint16 plen = tvb_get_ntohs(tvb, XIPH_PLEN);
+
+	memset(&dst, 0, sizeof dst);
+	memset(&src, 0, sizeof src);
+
+	if (hlen < XIPH_MIN_LEN) {
+		col_add_fstr(pinfo->cinfo, COL_INFO,
+		   "Bad XIP header length (%u, should be at least %u)",
+		    hlen, XIPH_MIN_LEN);
+
+		return 0;
+	}
+
+	col_set_str(pinfo->cinfo, COL_PROTOCOL, "XIP");
+	col_set_str(pinfo->cinfo, COL_INFO, "XIP Request");
+
+	if (tree) {
+
+		/* Construct protocol tree. */
+		ti = proto_tree_add_item(tree, proto_xip, tvb, 0, hlen, ENC_NA);
+
+		xip_tree = proto_item_add_subtree(ti, ett_xip);
+
+		/* Add header fields to tree. */
+		proto_tree_add_item(xip_tree, hf_xip_version, tvb,
+				    XIPH_VERS, 1, ENC_BIG_ENDIAN);
+
+		proto_tree_add_item(xip_tree, hf_xip_next_hdr, tvb,
+				    XIPH_NXTH, 1, ENC_BIG_ENDIAN);
+
+		proto_tree_add_uint_format(xip_tree, hf_xip_payload_len,
+ 		       				tvb, XIPH_PLEN, 2, plen,
+				      "Payload Length: %u bytes", plen);
+
+		proto_tree_add_item(xip_tree, hf_xip_hop_limit, tvb,
+				   	  XIPH_HOPL, 1, ENC_BIG_ENDIAN);
+
+		proto_tree_add_item(xip_tree, hf_xip_num_dst, tvb,
+				   	  XIPH_NDST, 1, ENC_BIG_ENDIAN);
+
+		proto_tree_add_item(xip_tree, hf_xip_num_src, tvb,
+				    	  XIPH_NSRC, 1, ENC_BIG_ENDIAN);
+
+		proto_tree_add_item(xip_tree, hf_xip_last_node, tvb,
+				    	  XIPH_LSTN, 1, ENC_BIG_ENDIAN);
+
+		/* Construct Destination DAG subtree. */
+		ti = proto_tree_add_item(xip_tree, hf_xip_dst_dag,
+		 tvb, XIPH_DSTD, dst_nodes * NODE_SIZE, ENC_BIG_ENDIAN);
+
+		dst_tree = proto_item_add_subtree(ti, ett_xip_ddag);
+
+		tvb_memcpy(tvb, (guint8 *)(&dst), XIPH_DSTD,
+						 NODE_SIZE * dst_nodes);
+
+		xia_ntop(&dst, dst_string, XIA_MAX_STRADDR_SIZE, 1);
+
+		display_dag(dst_tree, hf_xip_dst_dag_entry, tvb,
+						 XIPH_DSTD, dst_string);
+
+		/* Construct Source DAG subtree. */
+		ti = proto_tree_add_item(xip_tree, hf_xip_src_dag, tvb,
+		     src_offset, src_nodes * NODE_SIZE, ENC_BIG_ENDIAN);
+
+		src_tree = proto_item_add_subtree(ti, ett_xip_sdag);
+
+		tvb_memcpy(tvb, (guint8 *)(&src), src_offset,
+						NODE_SIZE * src_nodes);
+
+		xia_ntop(&src, src_string, XIA_MAX_STRADDR_SIZE, 1);
+
+		display_dag(src_tree, hf_xip_src_dag_entry, tvb,
+						src_offset, src_string);
+	}
+
+	next_tvb = tvb_new_subset(tvb, hlen, -1, -1);
+	call_dissector(udp_handle, next_tvb, pinfo, tree);
+
+	return tvb_length(tvb);
+}
+
+void
+proto_register_xip(void)
+{
+
+	static hf_register_info hf[] = {
+
+		{ &hf_xip_version,
+		{ "Version", "xip.version", FT_UINT8,
+		   BASE_DEC, NULL, 0x0,	NULL, HFILL }},
+
+		{ &hf_xip_next_hdr,
+		{ "Next Header", "xip.next_hdr", FT_UINT8,
+		   BASE_DEC|BASE_EXT_STRING, (&ipproto_val_ext),
+		   0x0, NULL, HFILL }},
+
+		{ &hf_xip_payload_len,
+		{ "Payload Length", "xip.payload_len", FT_UINT16,
+		   BASE_DEC, NULL, 0x0,	NULL, HFILL }},
+
+		{ &hf_xip_hop_limit,
+		{ "Hop Limit", "xip.hop_limit", FT_UINT8,
+		   BASE_DEC, NULL, 0x0,	NULL, HFILL }},
+
+		{ &hf_xip_num_dst,
+		{ "Number of Destination Nodes", "xip.num_dst", FT_UINT8,
+		   BASE_DEC, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_xip_num_src,
+		{ "Number of Source Nodes", "xip_num_src", FT_UINT8,
+		   BASE_DEC, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_xip_last_node,
+		{ "Last Node", "xip.last_node", FT_UINT8,
+		   BASE_DEC, NULL, 0x0,	NULL, HFILL }},
+
+		{ &hf_xip_dst_dag,
+		{ "Destination DAG", "xip.dst_dag", FT_NONE,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_xip_dst_dag_entry,
+		{ "Destination DAG Entry", "xip.dst_dag_entry", FT_STRING,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_xip_src_dag,
+		{ "Source DAG", "xip.src_dag", FT_NONE,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }},
+
+		{ &hf_xip_src_dag_entry,
+		{ "Source DAG Entry", "xip.src_dag_entry", FT_STRING,
+		   BASE_NONE, NULL, 0x0, NULL, HFILL }}
+	};
+
+	static gint *ett[] = {
+		&ett_xip,
+		&ett_xip_ddag,
+		&ett_xip_sdag
+	};
+
+
+	proto_xip = proto_register_protocol(
+		"eXpressive Internet Protocol",
+		"XIP",
+	        "xip");
+
+	proto_register_field_array(proto_xip, hf, array_length(hf));
+	proto_register_subtree_array(ett, array_length(ett));
+
+}
+
+void
+proto_reg_handoff_xip(void)
+{
+	dissector_handle_t xip_handle;
+	xip_handle = new_create_dissector_handle(dissect_xip, proto_xip);
+	dissector_add_uint("ethertype", ETHERTYPE_XIP, xip_handle);
+
+	udp_handle = find_dissector("udp");
+}
+
